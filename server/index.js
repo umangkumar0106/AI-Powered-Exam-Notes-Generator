136709faea1f412d05062d9e8eac185b87f4e5a7
Umang Kumar <uk9634129129@gmail.com>
2026-04-10 11:41:08 +0530

    first

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use("/api/credit", creditRouter)
 
 if (isProduction) {
   app.use(express.static(path.join(__dirname, "../client/dist")))
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"))
   })
 }
