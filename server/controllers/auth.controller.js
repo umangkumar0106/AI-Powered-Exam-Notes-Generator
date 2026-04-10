ac5a534b9cf3aa2af65d3e9d9baa8cc7db3683c3
Umang Kumar <uk9634129129@gmail.com>
2026-04-10 13:16:15 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -26,8 +26,8 @@ export const logOut = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({ message: "LogOut Successfully" });
   } catch (error) {
